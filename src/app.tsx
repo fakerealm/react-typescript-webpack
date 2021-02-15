483ccab3baaf425bdbd2aa949000ea3302249e90
Kunal Saini <babita79saini@gmail.com>
2021-02-15 09:47:01 +0530

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./main.scss";
+
+export default class App extends Component{
+	render(){
+		return(
+			<>
+				<h1 className="heading"> Thanks for using this template. </h1>
+				<h3>Edit src/App.tsx and create your own react project.</h3>
+			</>
+		);
+	}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,57 @@
 const path = require('path')
 
-module.exports = {
-  entry: "./build/app.js",
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js'
-  },
-  module: {
-  	rules: [{
-  		test: /\.m?js$/,
-  		exclude: /(node_modules|bower_components)/,
-  		use: {
-  			loader: 'babel-loader',
-  			options: {
-  				presets: ['@babel/preset-env'],
-				plugins: ["babel-plugin-loop-optimizer"]
-  			}
-  		}
-  	},{
-		  test: /\.css$/,
-		  exclude: /(node_modules|bower_components)/,
-		  use: ["css-loader", "style-loader"]
-	  }]
-  }
+const config = {
+	entry: "./src/index.js",
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'app.bundle.js'
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		module: {
+			rules: [{
+					test: /\.m?js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+						}
+					}
+				}, {
+					test: /\.tsx$/,
+					exclude: /(node_modules|bower_components)/,
+					use: "babel-loader"
+				},
+				{
+					test: /\.ts$/,
+					exclude: /(node_modules|bower_components)/,
+					use: "babel-loader"
+				},
+				{
+					test: /\.css$/,
+					exclude: /(node_modules|bower_components)/,
+					use: ["css-loader", "style-loader"]
+				}, {
+					test: /\.s[ac]ss$/i,
+					use: [
+						// Creates `style` nodes from JS strings
+						"style-loader",
+						// Translates CSS into CommonJS
+						"css-loader",
+						// Compiles Sass to CSS
+						"sass-loader",
+					],
+				}
+			]
+		}
+}
 
+module.exports = (env, argv) => {
+	if(argv.mode === "development"){
+		config.watch = true;
+	}
+
+	return config;
 }
